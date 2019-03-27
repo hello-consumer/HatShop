d3c70646dd89dff8c77fb57c1f2b028e111c7058
Joe Johnson <joe.wendell.johnson@gmail.com>
2019-03-27 14:00:50 -0500

    Order and Order Item entities

--- HatShop/Data/ApplicationDbContext.cs
+++ HatShop/Data/ApplicationDbContext.cs
@@ -27,5 +27,8 @@ namespace HatShop.Data
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
 
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
     }
 }
---
+++ HatShop/Data/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HatShop.Data
+{
+    public class OrderItem
+    {
+        public int ID { get; set; }
+        public int Quantity { get; set; }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public string OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
