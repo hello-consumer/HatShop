6901e64019314a08b2fe01f36adc703460f2a6a4
Joe Johnson <joe.wendell.johnson@gmail.com>
2019-03-27 14:19:33 -0500

    Checkout logic using Entity Framework

--- HatShop/Controllers/CheckoutController.cs
+++ HatShop/Controllers/CheckoutController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using HatShop.Data;
 using HatShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HatShop.Controllers
 {
@@ -27,24 +28,61 @@ namespace HatShop.Controllers
         {
             if (this.ModelState.IsValid)
             {
-                //TODO: Get a lot more info here, validate credit card + address, save it to a database
+                string cookieData = Request.Cookies.ContainsKey("HatShopCartInfo") ? Request.Cookies["HatShopCartInfo"] : null;
 
-                Order order = new Order();
-                order.ID = Guid.NewGuid().ToString().Substring(0, 8);
-                order.OrderDate = DateTime.Now.ToString();
-                order.ContactEmail = model.ContactEmail;
-                order.ContactName = model.ContactName;
-                order.ContactPhoneNumber = model.ContactPhoneNumber;
-                order.ShippingCity = model.ShippingCity;
-                order.ShippingCountry = model.ShippingCountry;
-                order.ShippingPostalCode = model.ShippingPostalCode;
-                order.ShippingRegion = model.ShippingRegion;
-                order.ShippingStreet1 = model.ShippingStreet1;
-                order.ShippingStreet2 = model.ShippingStreet2;
-                _context.Orders.Add(order);
-                _context.SaveChanges();
+                Guid cookieIdentifier;
+                if (Guid.TryParse(cookieData, out cookieIdentifier))
+                {
+                    Cart cart = _context.Carts
+                            .Include(c => c.CartItems)
+                            .ThenInclude(ci => ci.Product)
+                            .ThenInclude(p => p.ProductColors)
+                            .Include(c => c.CartItems)
+                            .ThenInclude(ci => ci.Product)
+                            .ThenInclude(p => p.ProductSizes)
+                            .FirstOrDefault(c => c.CookieIdentifier == cookieIdentifier);
 
-                return RedirectToAction("index", "receipt", new { id = order.ID });
+                    if (cart != null)
+                    {
+
+                        //TODO: Get a lot more info here, validate credit card + address, save it to a database
+
+                        Order order = new Order();
+                        order.ID = Guid.NewGuid().ToString().Substring(0, 8);
+                        order.OrderDate = DateTime.Now.ToString();
+                        order.ContactEmail = model.ContactEmail;
+                        order.ContactName = model.ContactName;
+                        order.ContactPhoneNumber = model.ContactPhoneNumber;
+                        order.ShippingCity = model.ShippingCity;
+                        order.ShippingCountry = model.ShippingCountry;
+                        order.ShippingPostalCode = model.ShippingPostalCode;
+                        order.ShippingRegion = model.ShippingRegion;
+                        order.ShippingStreet1 = model.ShippingStreet1;
+                        order.ShippingStreet2 = model.ShippingStreet2;
+
+
+                        order.OrderItems = cart.CartItems.Select(ci => new OrderItem
+                        {
+                            ProductID = ci.ProductID,
+                            Color = ci.ProductColor != null ? ci.ProductColor.Color : null,
+                            Description = ci.Product.Description,
+                            Name = ci.Product.Name,
+                            Price = ci.Product.Price,
+                            Quantity = ci.Quantity,
+                            Size = ci.ProductSize != null ? ci.ProductSize.Size : null
+                        }).ToArray();
+
+                        _context.CartItems.RemoveRange(cart.CartItems);
+                        _context.Carts.Remove(cart);
+                        Response.Cookies.Delete("HatShopCartInfo");
+                        _context.Orders.Add(order);
+                        _context.SaveChanges();
+
+                        return RedirectToAction("index", "receipt", new { id = order.ID });
+                    }
+                    ModelState.AddModelError("cart", "There was a problem processing your cart");
+                }
+
             }
             return View(model);
         }
