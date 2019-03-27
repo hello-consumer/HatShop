e643d1575cf685645dc1168247c0f169628b0dc7
Joe Johnson <joe.wendell.johnson@gmail.com>
2019-03-27 14:07:15 -0500

    Copying ViewModel data to a data entity

--- HatShop/Controllers/CheckoutController.cs
+++ HatShop/Controllers/CheckoutController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HatShop.Data;
 using HatShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,13 @@ namespace HatShop.Controllers
 {
     public class CheckoutController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CheckoutController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,10 +27,24 @@ namespace HatShop.Controllers
         {
             if (this.ModelState.IsValid)
             {
-                Console.WriteLine("Processing an order from " + model.ContactEmail);
                 //TODO: Get a lot more info here, validate credit card + address, save it to a database
 
-                return RedirectToAction("index", "receipt");
+                Order order = new Order();
+                order.ID = Guid.NewGuid().ToString().Substring(0, 8);
+                order.OrderDate = DateTime.Now.ToString();
+                order.ContactEmail = model.ContactEmail;
+                order.ContactName = model.ContactName;
+                order.ContactPhoneNumber = model.ContactPhoneNumber;
+                order.ShippingCity = model.ShippingCity;
+                order.ShippingCountry = model.ShippingCountry;
+                order.ShippingPostalCode = model.ShippingPostalCode;
+                order.ShippingRegion = model.ShippingRegion;
+                order.ShippingStreet1 = model.ShippingStreet1;
+                order.ShippingStreet2 = model.ShippingStreet2;
+                _context.Orders.Add(order);
+                _context.SaveChanges();
+
+                return RedirectToAction("index", "receipt", new { id = order.ID });
             }
             return View(model);
         }
