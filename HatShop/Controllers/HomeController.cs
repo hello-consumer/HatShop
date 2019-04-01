a49319bf57ff1e4f3c6f81b040b6a875387a7291
Joe Johnson <joe.wendell.johnson@gmail.com>
2019-04-01 12:59:24 -0500

    Sample API calls

--- HatShop/Controllers/HomeController.cs
+++ HatShop/Controllers/HomeController.cs
@@ -6,11 +6,69 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HatShop.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace HatShop.Controllers
 {
     public class HomeController : Controller
     {
+        private string _myApiKey;
+        private ILogger<HomeController> _logger;
+
+        public HomeController(IConfiguration configuration, ILogger<HomeController> logger)
+        {
+            _myApiKey = configuration.GetValue<string>("myApiKey");
+            _logger = logger;
+        }
+
+        public IActionResult Bored()
+        {
+
+            string endpoint = "https://www.boredapi.com/api/activity?myApiKey=" + _myApiKey;
+            try
+            {
+                System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
+                string response = httpClient.GetStringAsync(endpoint).Result;
+                BoredViewModel typedResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<BoredViewModel>(response);
+                return View(typedResponse);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return Content("Can't get this right now");
+            }
+        }
+
+        public IActionResult Stocks()
+        {
+            string endpoint = "https://api.iextrading.com/1.0/ref-data/symbols";
+            try
+            {
+                System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
+                string response = httpClient.GetStringAsync(endpoint).Result;
+                var typedResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Newtonsoft.Json.Linq.JObject>>(response);
+                var stocks = typedResponse.Select(stock => new Stock
+                {
+                    Symbol = stock.GetValue("symbol").ToString(),
+                    Name = stock.GetValue("name").ToString()
+                }).ToArray();
+                return Json(stocks);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return Content("Can't get this right now");
+            }
+
+        }
+
+        private class Stock
+        {
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
---
+++ HatShop/Models/BoredViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HatShop.Models
+{
+    public class BoredViewModel
+    {
+        public string Activity { get; set; }
+        public decimal Accessibility { get; set; }
+        public string Type { get; set; }
+        public int Participants { get; set; }
+        public decimal Price { get; set; }
+        public string Key { get; set; }
+    }
+}
